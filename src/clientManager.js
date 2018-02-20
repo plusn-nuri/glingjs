83005d9689799863d56c79f370acf639c6615988
plusn-nuri <nuri@plusnconsulting.com>
2018-02-20 13:43:01 -0800

    using class

--- src/clientManager.js
+++ src/clientManager.js
@@ -1,14 +1,15 @@
 'use strict';
 
-var ChangeType = require('./changeType');
+const ChangeType = require('./changeType');
 
-var ClientManager = function ClientManager(config) {
-    this.config = config;
+class ClientManager {
+    constructor(config) {
+        this.config = config;
+        this.clientTopicMap = this.createTopicMap(config);
+    }
 
-    this.clientTopicMap = {}
-
-    this.createTopicMap = function (config) {
-        var result = {};
+    createTopicMap(config) {
+        const result = {};
         config.listeners.forEach(listener => {
 
             result[listener.topic] || (result[listener.topic] = []);
@@ -17,49 +18,46 @@ var ClientManager = function ClientManager(config) {
         return result;
     }
 
-    this.clientTopicMap = this.createTopicMap(config);
-
-    this.broadcast = function (topic, payload) {
-
+    broadcast(topic, payload) {
         const clients = this.clientTopicMap[topic];
-        const message = (typeof(payload) === "string") && payload || JSON.stringify(payload);
+        const message = (typeof (payload) === "string") && payload || JSON.stringify(payload);
         clients.forEach(c => {
             if (c.connected) {
                 c.sendUTF(message);
             }
         })
     }
-}
 
-ClientManager.prototype.removeConnection = function (connection, topic) {
-    const map = this.clientTopicMap;
+    removeConnection(connection, topic) {
+        const map = this.clientTopicMap;
 
-    var offset = map[topic].findIndex(e => e == connection);
+        const offset = map[topic].findIndex(e => e == connection);
 
-    if (offset !== -1) {
-        map[topic].splice(offset, 1);
+        if (offset !== -1) {
+            map[topic].splice(offset, 1);
+        }
     }
-}
 
-ClientManager.prototype.registerClientConnection = function (request, clientTopicMap) {
-    var that = this;
-    const map = clientTopicMap || this.clientTopicMap;
-    const topic = request.requestedProtocols[0];
+    registerClientConnection(request, clientTopicMap) {
+        var that = this;
+        const map = clientTopicMap || this.clientTopicMap;
+        const topic = request.requestedProtocols[0];
 
-    var connection = request.accept(topic, request.origin);
+        var connection = request.accept(topic, request.origin);
 
-    connection.on('close', function (reasonCode, description) {
-        ClientManager.prototype.removeConnection.call(that, connection, topic);
+        connection.on('close', function (reasonCode, description) {
+            ClientManager.prototype.removeConnection.call(that, connection, topic);
 
-        console.log(`Closed connection  ${connection.remoteAddress} because ${reasonCode} ${description}`);
-        connection = null;
-    });
+            console.log(`Closed connection  ${connection.remoteAddress} because ${reasonCode} ${description}`);
+            connection = null;
+        });
 
-    this.clientTopicMap[topic].push(connection);
+        this.clientTopicMap[topic].push(connection);
 
-    console.log(`Client accepted from ${connection.remoteAddress} for topic "${topic}".`)
+        console.log(`Client accepted from ${connection.remoteAddress} for topic "${topic}".`)
 
-    return { topic, connection };
+        return { topic, connection };
+    }
 }
 
 module.exports = ClientManager;
